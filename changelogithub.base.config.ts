a50327dc1e8be5983e32c50adb5362874ad9bdc5
dprevost-perso <dprevost-github@outlook.com>
2026-03-25 18:53:55 -0400

    ci: fix weir GitHub release titles

--- changelogithub.base.config.ts
+++ changelogithub.base.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'changelogithub'
 
 export default defineConfig({
   types: {
-    feat: { title: '### &nbsp;&nbsp;&nbsp;🚀 Features' },
-    fix: { title: '### &nbsp;&nbsp;&nbsp;🐞 Bug Fixes' },
-    perf: { title: '### &nbsp;&nbsp;&nbsp;🏎 Performance' },
-    chore: { title: '### &nbsp;&nbsp;&nbsp;🏡 Other Changes' },
-    docs: { title: '### &nbsp;&nbsp;&nbsp;🏡 Other Changes' },
-    refactor: { title: '### &nbsp;&nbsp;&nbsp;🏡 Other Changes' }
+    feat: { title: '🚀 Features' },
+    fix: { title: '🐞 Bug Fixes' },
+    perf: { title: '🏎 Performance' },
+    chore: { title: '🏡 Other Changes' },
+    docs: { title: '🏡 Other Changes' },
+    refactor: { title: '🏡 Other Changes' }
   },
   titles: {
-    breakingChanges: '### &nbsp;&nbsp;&nbsp;🚨 Breaking Changes'
+    breakingChanges: '🚨 Breaking Changes'
   }
 })
