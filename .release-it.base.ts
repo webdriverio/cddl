9fede3ba3cc6a06bf7d5878f9d939ad69db756b3
dprevost-perso <dprevost-github@outlook.com>
2026-03-25 21:53:34 -0400

    fix GitHub release not scoping to packages commits

--- .release-it.base.ts
+++ .release-it.base.ts
@@ -2,18 +2,24 @@ import type { Config } from 'release-it';
 
 const baseConfig = (name: string): Config => ({
   plugins: {
-    "release-it-pnpm": {}
+    "release-it-pnpm": {
+      // Disable the release step here so we can use the native one below
+      disableRelease: true,
+    }
   },
   git: {
     requireCleanWorkingDir: false,
     addUntrackedFiles: true,
-    // Must be overriden by each package to have seperate tags and changelogs
     tagName: `${name}-v\${version}`,
     commitMessage: `chore(${name}): release v\${version}`,
+    // This filtered command WILL be used by the native github plugin
     changelog: 'git log --pretty=format:"* %s (%h)" ${latestTag ? latestTag + "..HEAD" : ""} -- . ../../tsconfig.json'
   },
   github: {
     release: true,
+    // explicitly null ensures it uses the git.changelog output defined above
+    releaseNotes: null,
+    releaseName: `${name}-v\${version}`,
   }
 });
 
