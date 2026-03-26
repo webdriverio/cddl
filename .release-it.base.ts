8bbadb174a0659468bbb141390fd76a4058921dc
dprevost-perso <dprevost-github@outlook.com>
2026-03-25 21:33:18 -0400

    fix bad github releases

--- .release-it.base.ts
+++ .release-it.base.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'release-it';
 
-const baseConfig: Config = {
+const baseConfig = (name: string): Config => ({
   plugins: {
     "release-it-pnpm": {}
   },
@@ -8,13 +8,13 @@ const baseConfig: Config = {
     requireCleanWorkingDir: false,
     addUntrackedFiles: true,
     // Must be overriden by each package to have seperate tags and changelogs
-    // tagName: "packageName-v${version}",
-    // commitMessage: "chore(packageName): release v${version}",
+    tagName: `${name}-v\${version}`,
+    commitMessage: `chore(${name}): release v\${version}`,
     changelog: 'git log --pretty=format:"* %s (%h)" ${latestTag ? latestTag + "..HEAD" : ""} -- . ../../tsconfig.json'
   },
   github: {
     release: true,
   }
-};
+});
 
 export default baseConfig;
--- packages/cddl/.release-it.ts
+++ packages/cddl/.release-it.ts
@@ -2,14 +2,7 @@ import baseConfig from '../../.release-it.base';
 import type { Config } from 'release-it';
 
 const config: Config = {
-  ...baseConfig,
-  git: {
-    ...baseConfig.git,
-    // Must be overriden by each package to have seperate tags and changelogs
-    tagName: "cddl-v${version}",
-    tagMatch: "cddl-v[0-9]*",
-    commitMessage: "chore(cddl): release v${version}",
-  },
+  ...baseConfig('cddl'),
 };
 
 export default config;
